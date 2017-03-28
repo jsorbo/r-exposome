916baedcddda32439c82885abf01d2fc5ddcbcbc
Jeff Sorbo <jsorbo@hubbell.com>
2017-03-28 15:59:41 -0400

    updated quantilefunctions

--- r/quantileFunctions.R
+++ r/quantileFunctions.R
@@ -5,16 +5,16 @@ quints <- within(merged, quintile <- as.integer(cut(educationHighSchoolOrAboveRa
 
 quints
 
-append_quintile_column <- function(df, column) {
-  df <- within(df, new_quintile <- as.integer(cut(df[,column], quantile(df[,column], probs=0:5/5), include.lowest=TRUE)))
-  new_col_name <- paste0(column, "_quintile")
-  names(df)[ncol(df)] <- new_col_name
-  return(df)
-}
+# append_quintile_column <- function(df, column) {
+#   df <- within(df, new_quintile <- as.integer(cut(df[,column], quantile(df[,column], probs=0:5/5), include.lowest=TRUE)))
+#   new_col_name <- paste0("Quintile.", column)
+#   names(df)[ncol(df)] <- new_col_name
+#   return(df)
+# }
 
 append_ntile_column <- function(df, column, n) {
   df <- within(df, new_quintile <- as.integer(cut(df[,column], quantile(df[,column], probs=0:n/n), include.lowest=TRUE)))
-  new_col_name <- paste0(column, "_", n, "tile")
+  new_col_name <- paste0(n, "tile.", column)
   names(df)[ncol(df)] <- new_col_name
   return(df)
 }
@@ -26,12 +26,12 @@ append_quintile_column <- function(df, column) {
 convert_quintile_to_binary <- function(df, column) {
   for (i in 1:5) {
     df <- within(df, new_col <- as.integer(ifelse(df[column] == i, 1, 0)))
-    new_col_name <- paste0(column, "_", i)
+    new_col_name <- paste0(column, ".", i)
     names(df)[ncol(df)] <- new_col_name
   }
   return(df)
 }
 
 merged <- append_quintile_column(merged, "educationHighSchoolOrAboveRate")
-merged <- convert_quintile_to_binary(merged, "educationHighSchoolOrAboveRate_5tile")
+merged <- convert_quintile_to_binary(merged, "5tile.educationHighSchoolOrAboveRate")
 merged
